f016d82e05dac1e50d271c1c570547c8e2a75825
Prashant Bhandari <79316332+bhandariprashant@users.noreply.github.com>
2021-07-13 14:10:05 -0400

    Create basic_point.R

---
+++ basic_point.R
@@ -0,0 +1,18 @@
+p<- ggplot(dat1) + 
+  geom_col(aes(x =MAF_Class , y = Proportion), size = 1,width=0.5) +
+  labs(x="Minor Allele Frequency Class") + ylim(c(0,50))+scale_color_npg()+
+   theme(axis.text.y   = element_text(size=14),
+        axis.text.x   = element_text(size=14),
+        axis.title.y  = element_text(size=14),
+        axis.title.x  = element_text(size=14),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)
+  )
+
+  
+p+coord_flip()
+
+ggsave("maf.png", width = 10, height = 7.5, device='png', dpi=700)
